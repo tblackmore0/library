1548dff799383c17c1be5ee8bad5468fc8e42172
tblackmore0 <tblackmore0@gmail.com>
2021-02-28 00:25:54 +0000

    init all files

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+let myLibrary = [];
 
+function newBook (title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        return '${title} by ${author}, ${pages} pages, ${read}.'
+    }
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function displayBooks () {
+    for (i=0, i < myLibrary.length, i++) {
+
+    }
+}
+
+const bookSpace = document.querySelector('.bookSpace');
+
+function createCell () {
+    const cell = document.createElement('div')
+    bookSpace.appendChild(cell).className = 'cell'
+}
